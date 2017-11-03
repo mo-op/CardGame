f2abc3912273afdba52d17a41d78a4fc4663b1fc
Robin <robin.neu@devinci.fr>
2017-11-03 23:33:02 +0100

    Restructured view/controller

---
+++ src/main/java/fr/esilv/fsociety/cardgame/Controller/MenuController.java
@@ -0,0 +1,19 @@
+package fr.esilv.fsociety.cardgame.Controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MenuController extends Application {
+
+    public MenuController(){
+
+    }
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
--- src/main/java/fr/esilv/fsociety/cardgame/Launcher.java
+++ src/main/java/fr/esilv/fsociety/cardgame/Launcher.java
@@ -1,10 +1,24 @@
 package fr.esilv.fsociety.cardgame;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
 
-public class Launcher {
+public class Launcher extends Application{
 
-	
-	public static void main(String[] args) {
-		System.out.print("Hello! Your install is succesfull");
-	}
-	
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(Launcher.class.getResource("/fr.esilv.fsociety.cardgame/MenuOverview.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Menu");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
